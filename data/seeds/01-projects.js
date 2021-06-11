51420ebd8ab8e03d9523c5119b2e6fe1d546c4cf
gavin-rilee <you@example.com>
2021-06-11 12:25:28 -0400

    finsihed migrations and 2 seeds

---
+++ data/seeds/01-projects.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+      return knex('project').insert([
+        {project_id: 1, project_name: 'destroy the world',
+          project_description: 'DESTROY EARTH AND REBUILD', project_completed:false},
+
+        {project_id: 2, project_name: 'get snurch to talk in english', 
+        project_description: 'Teach the snurch english', project_completed:false},
+        
+      ]);
+    
+};
